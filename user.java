72bdf00ef4471feda0aed2a724ad3eb64b6d7f71
Shuchita189 <68604143+Shuchita189@users.noreply.github.com>
2024-07-16 17:21:05 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,11 @@
+package com.blog.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    // Getters and Setters
+}

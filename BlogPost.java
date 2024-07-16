37b9865d81db28ed138b4e66c4768803c61deef6
Shuchita189 <68604143+Shuchita189@users.noreply.github.com>
2024-07-16 17:24:41 +0530

    Create BlogPost.java

---
+++ BlogPost.java
@@ -0,0 +1,10 @@
+package com.blog.model;
+
+public class BlogPost {
+    private int id;
+    private String title;
+    private String content;
+    private String imageUrl;
+
+    // Getters and Setters
+}
